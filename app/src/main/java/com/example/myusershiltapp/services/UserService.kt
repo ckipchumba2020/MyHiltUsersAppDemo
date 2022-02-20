a0f87c238a5ea538882ec8868a10050e4afdfc10
CK <collins@analence.com>
2022-02-20 21:58:00 +0300

    corrected data type from api

--- app/src/main/java/com/example/myusershiltapp/services/UserService.kt
+++ app/src/main/java/com/example/myusershiltapp/services/UserService.kt
@@ -1,5 +1,6 @@
 package com.example.myusershiltapp.services
 
+import com.example.myusershiltapp.models.User
 import com.example.myusershiltapp.models.UserResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -7,5 +8,5 @@ import retrofit2.http.GET
 interface UserService {
 
     @GET("/users")
-    suspend fun getUsers(): Response<UserResponse>
+    suspend fun getUsers(): Response<List<User>>
 }
